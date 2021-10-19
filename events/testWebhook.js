5391b5df8b12923f5aba417a5daf746e95389c45
Hallel Shapiro <hallelshapiro@gmail.com>
2021-10-19 16:46:03 -0400

    Removed test webhooks

--- events/testWebhook.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = require('../index')
-
-client.on('ready', () => {
-    const wc = new Discord.WebhookClient({
-        url: 'https://discord.com/api/webhooks/895774654663360562/TTvlPOKjEq90M_KGAZnKN4k61IVlkCAuW4ZrU7antgrjbcRDziZz7T0X36C7t9IEi9YX'
-    })
-    wc.send({content: 'yes'})
-})
